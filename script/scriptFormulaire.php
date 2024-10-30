faa2da91ae803a4b85b75169bea5466c1709a079
Najib bar <barnaj1910@gmail.com>
2024-10-30 22:53:59 +0100

    modif page principale

--- script/scriptFormulaire.php
+++ script/scriptFormulaire.php
@@ -2,17 +2,16 @@
 // http://londres.uca.local/~nabarbara/cache/
 // mettre le fichier php dans public_html/
 
-if(empty($_POST['nom'])) { // Ici on définit ce qu’on fait en cas d’erreur
+if(empty($_POST['nom'])) {
     echo '<p class="erreur">Le nom doit être renseigné !</p>';
 }
 else { 
     echo 'Bonjour ' . $_POST['nom'] . ' ! ';
 }
 
-if(empty($_POST['email'])) { // Ici on définit ce qu’on fait en cas d’erreur
+if(empty($_POST['email'])) { 
     echo '<p class="erreur">Adresse email doit être renseigné !</p>';
 }
-
 else { 
     echo 'Votre adresse mail est ' . $_POST['email'] . ' ! ';
 }
