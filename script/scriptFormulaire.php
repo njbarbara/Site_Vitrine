f94828b6f2137b88f7756a83ba478016f1d6676a
Najib bar <barnaj1910@gmail.com>
2024-11-02 16:29:20 +0100

    ajout des media query + modif script php + fin du tab

--- script/scriptFormulaire.php
+++ script/scriptFormulaire.php
@@ -2,21 +2,47 @@
 // http://londres.uca.local/~nabarbara/cache/
 // mettre le fichier php dans public_html/
 
+$erreur = 0;
 if(empty($_POST['nom'])) {
-    echo '<p class="erreur">Le nom doit être renseigné !</p>';
+    echo 'Le nom doit être renseigné !';
+    $erreur = 1;
 }
 else { 
     echo 'Bonjour ' . $_POST['nom'] . ' ! ';
 }
 
 if(empty($_POST['email'])) { 
-    echo '<p class="erreur">Adresse email doit être renseigné !</p>';
+    echo 'Adresse email doit être renseigné !';
+    $erreur = 1;
 }
 else { 
-    echo 'Votre adresse mail est ' . $_POST['email'] . ' ! ';
+    if (filter_var(value: $_POST['email'], filter: FILTER_VALIDATE_EMAIL)) {
+        echo 'Votre adresse mail est ' . $_POST['email'] . ' ! ';
+    }
+    else{
+        echo 'adresse mail au format incorrect';
+        $erreur = 1;
+    }
 }
 
-//utiliser filtervar();
+if (empty($_POST['msg'])) {
+    echo 'Le message doit etre renseigné';
+    $erreur = 1;
+} 
+else {
+    echo 'Votre message est ' . $_POST['msg'] . '.';
+}
 
+if(empty($_POST['creneau'])){
+    echo 'Le créneau doit etre renseigné ';
+    $erreur = 1;
+}
+else{
+    echo 'Votre créneau est : ' .  $_POST['creneau'] . '.';
+}
+
+if($erreur == 0){
+    echo 'Erreur, refaire le formulaiure';
+}
 
 ?>
